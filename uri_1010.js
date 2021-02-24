fecba018b50d752c8ffe2136e2183937521575bd
Adalberto Maiam <btowmaia@gmail.com>
2021-02-24 19:37:10 -0300

    anothers uri's until 1010

--- uri_1010.js
+++ uri_1010.js
@@ -10,9 +10,9 @@ let codigo1 = parseInt(item1.shift());
 let nPecas1 = parseInt(item1.shift());
 let valorUni1 = parseFloat(item1.shift());
 
-let codigo2 = parseInt(item2.shift(''));
-let nPecas2 = parseInt(item2.shift(''));
-let valorUni2 = parseFloat(item2.shift(''));
+let codigo2 = parseInt(item2.shift());
+let nPecas2 = parseInt(item2.shift());
+let valorUni2 = parseFloat(item2.shift());
 
 total = (nPecas1*valorUni1)+(nPecas2*valorUni2);
 console.log("VALOR A PAGAR: R$ "+total.toFixed(2));
